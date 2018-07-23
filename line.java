5d26bbd834dd3d44d1aad1b673683d36ace0e6d1
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-23 10:43:56 +0530

    Create line.java

---
+++ line.java
@@ -0,0 +1,10 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s="hello.she is good girl.she helps lot of people";
+		String str[]=s.split("\\.");
+		int l=str.length;
+		System.out.print(l);
+	}
+}
