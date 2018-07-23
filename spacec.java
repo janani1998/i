b2cae0d1786408cff63038a063706bef0c05c2b2
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-23 10:33:33 +0530

    Create spacec.java

---
+++ spacec.java
@@ -0,0 +1,10 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s="she is good girl";
+		String str[]=s.split(" ");
+	            int l=str.length;
+		System.out.print(l-1);
+	}
+}
