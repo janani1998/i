644cbbdc7f34d1e4f3622536a65f5d443751d5b0
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-23 11:29:17 +0530

    Create numerinc.java

---
+++ numerinc.java
@@ -0,0 +1,9 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s="hello.she is good girl123.sara456";
+		s=s.replaceAll("[^0-9]","");
+		System.out.print(s.length());
+	}
+}
