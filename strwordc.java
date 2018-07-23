7b45bbffc06dff1882826bf476e5f4661a3b2865
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-23 10:19:09 +0530

    Create strwordc.java

---
+++ strwordc.java
@@ -0,0 +1,11 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		String s="hello world";
+        String str[]=s.split(" ");  
+		int l=str.length;
+		System.out.print(l);
+	}
+}
